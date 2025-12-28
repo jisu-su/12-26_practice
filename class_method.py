f3212da38953ab824d75b032e95667ce0b5a8a40
박지수 <trleresoo@gmail.com>
2025-12-28 22:28:04 +0900

    create 메서드 부분 수정

--- class_method.py
+++ class_method.py
@@ -6,9 +6,8 @@ class (Lyricsmanager):
     def __init__(self, filename):
         self.filename = filename # 이제 이 상자는 self.filename을 기억한다
 
-    # 3. create. 가사를 새로 쓰는 '메서드' 만들기
+    # 3. create 부분. 가사를 새로 쓰는 '메서드' 만들기
     def lyrics_write(self, text):
         # 여기에 'w' 모드 코드를 넣어 보기
-        pass with open(self.filename, "w", encoding="utf-8") as f:
-            f.write("반짝반짝 작은 별")
-            f.write("\n아름답게 비추네")
+        with open(self.filename, "w", encoding="utf-8") as f:
+            f.write(text)
