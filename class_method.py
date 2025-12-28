ee680b75563ee9d419651a75bcae6b4c1254b0d7
박지수 <trleresoo@gmail.com>
2025-12-28 22:43:34 +0900

    delete 메서드 만들기

--- class_method.py
+++ class_method.py
@@ -17,3 +17,15 @@ class (Lyricsmanager):
         # 여기에 'r' 모드 코드를 넣어 보기
         with open(self.filename, "r", encoding="utf-8") as f:
             return f.read()
+        
+    # 5. delete 부분. 특정 줄을 삭제하는 '메서드' 만들기
+    # 몇 번째 줄을 지울지(index)를 재료로 받아야 한다.
+    def lyrics_delete(self, index):
+        # 1. 파일 읽기 (readlines)
+        # 2. del lines[index]
+        # 3. 다시 쓰기 (writelines)
+        with open(self.filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            del lines[index]
+            with open(self.filename, "w", encoding="utf-8") as f2:
+                f2.writelines(lines)
